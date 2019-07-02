5466f9711d637c14cfebc1f971fa8fb56dc9f0d7
Fred Valle <phredvalle@gmail.com>
2019-07-02 16:05:56 -0500

    setting up initial animation state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, trigger, state, style } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  animations: [
+    trigger('divState', [
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0)'
+      })),
+      state('highlighted', style({
+        backgroundColor: 'blue',
+        transform: 'translateX(100px)'
+      }))
+    ])
+  ]
 })
+
 export class AppComponent {
+  list = ['Milk', 'Sugar', 'Bread'];
+  state = 'normal';
+
+  onAdd(item) {
+    this.list.push(item);
+  }
+
+  onDelete(item) {
+    this.list.splice(this.list.indexOf(item), 1);
+  }
 
 }
